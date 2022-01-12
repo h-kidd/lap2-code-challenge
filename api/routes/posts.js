4a0ac32de681311967a54e2b26bc5375b6910e38
“Ginger9307” <tvlezhneva@gmail.com>
2022-01-12 12:24:47 +0000

    add post saving

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const Post = require('../model/post')
 
-router.post('/', (req, res) => {
-    res.send(req.body);
+// post the post route
+router.post('/', async (req, res) => {
+    let today = new Date();
+    // create new post
+    const post = new Post({
+        title: req.body.title,
+        name: req.body.name,
+        date: today,
+        body: req.body.body
+    });
+    console.log(today, post);
+// save post to db
+    const savedPost = await post.save((err, result) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.send(result);
+        }
+    });
+
+
+
+
 })
 
 module.exports = router;
