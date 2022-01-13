129c70e40e0ad4c724ca421b0bde57c4613dbae3
Hayden Kidd <hkidd@live.co.uk>
2022-01-13 08:53:10 +0000

    client response changes

--- client/js/submit.js
+++ client/js/submit.js
@@ -4,21 +4,20 @@ myForm.addEventListener("submit", e => {
     const title = e.target.title.value;
     const name = e.target.username.value;
     const body = e.target.message.value;
-    const date = new Date().toISOString();
-    postMessage(title, name, body, date);
+    postMessage(title, name, body);
 });
 
-async function postMessage(title, name, body, date){
+async function postMessage(title, name, body){
     try {
-        const post = {title: title, name: name, date: date, body: body};
+        const post = {title: title, name: name, body: body};
         const options = {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(post)
         };
         const response = await fetch('http://localhost:3000/posts', options);
-        const id = await response.json();
-        window.open(window.location.href.slice(0, -11) + `/posts.html?id=${id}`, '_self');
+        const data = await response.json();
+        window.open(window.location.href.slice(0, -11) + `/posts.html?id=${data._id}`, '_self');
     } catch (err) {
         console.warn(err);
     };
